de98d3ae37632a737445ffa1247b352afbe65c1b
Xinyi Guo <xinyiguo@berkeley.edu>
2022-08-13 00:50:15 -0700

    Day 80

--- Leetcode/494. Target Sum.java
+++ Leetcode/494. Target Sum.java
@@ -1,31 +1,54 @@
-public class Solution {
-    int total;
+// Brute Force: backtracking
+// Time Complexity: O(2^N)
+
+class Solution {
+    int res;
     
-    public int findTargetSumWays(int[] nums, int S) {
-        total = Arrays.stream(nums).sum();
-        
-        int[][] memo = new int[nums.length][2 * total + 1];
-        for (int[] row : memo) {
-            Arrays.fill(row, Integer.MIN_VALUE);
-        }
-        return calculate(nums, 0, 0, S, memo);
+    public int findTargetSumWays(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return res;
     }
     
-    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
-        if (i == nums.length) {
-            if (sum == S) {
-                return 1;
-            } else {
-                return 0;
+    public void backtrack(int[] nums, int target, int i, int sum) {
+        if(i == nums.length) {
+            if(sum == target) {
+                res++;
             }
+            return;
         } else {
-            if (memo[i][sum + total] != Integer.MIN_VALUE) {
-                return memo[i][sum + total];
-            }
-            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
-            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
-            memo[i][sum + total] = add + subtract;
-            return memo[i][sum + total];
+            backtrack(nums, target, i + 1, sum + nums[i]);
+            backtrack(nums, target, i + 1, sum - nums[i]);
         }
+    }
+}
+
+// DP using 2D array
+// Time Complexity: O(N * T)
+// Space Complexity: O(N * T)
+// Use a 2D array to store possible number of assignments
+// horizontal direction is the nums[i] counted
+// vertical direction is +nums[i] to -nums[i] from an offset total
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = 0, N = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        int[][] dp = new int[nums.length][total * 2 + 1];   // Going up or down plus the offset
+        dp[0][total - nums[0]] = 1;
+        dp[0][total + nums[0]] += 1;
+        
+        for(int i = 1; i < N; i++) {
+            for(int sum = -total; sum <= total; sum++) {
+                // check if previous combination exists
+                // else risking the index out of bounds error
+                if (dp[i - 1][sum + total] > 0) {
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+        // dealing with the edge case that the target is greater or smaller than applying all '+' or all '-'
+        return Math.abs(target) > total? 0 : dp[N - 1][target + total];
     }
 }
